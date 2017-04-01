c4aaac50813ffa302a37a10c12e662043989739a
Leonardo <leonevesaraujo@gmail.com>
2017-04-01 19:40:47 -0300

    loginModel

--- APS_CHAT_CLIENT/client/model/LoginModel.java
+++ APS_CHAT_CLIENT/client/model/LoginModel.java
@@ -1,5 +1,19 @@
 package model;
 
 public class LoginModel {
+    private String errorMessage = "";
+    
+    public LoginModel(){
+    }
+    
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+    public boolean login(String user, String pwd){
+ 
+    }
+    
+    public boolean loginObjectRecieve(Request result){
 
+    }
 }
